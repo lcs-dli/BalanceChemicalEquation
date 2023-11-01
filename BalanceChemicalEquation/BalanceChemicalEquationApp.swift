d61ecbac80b18f8afb494f9964dde789533ee39c
David Li <dtli@lcs.on.ca>
2023-11-01 12:09:24 -0400

    Initial Commit

---
+++ BalanceChemicalEquation/BalanceChemicalEquationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BalanceChemicalEquationApp.swift
+//  BalanceChemicalEquation
+//
+//  Created by David Li on 2023-11-01.
+//
+
+import SwiftUI
+
+@main
+struct BalanceChemicalEquationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
